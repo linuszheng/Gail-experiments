61575678945bee856e17f8742058d56e89886ccd
Linus Zheng <linusjz@utexas.edu>
2023-09-27 20:32:16 -0500

    pdf likelihood calculation

---
+++ get_pdf_ll.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import re
+from scipy.stats import norm
+
+
+_data_path = "a-2d-merge-easy/data"
+_traj_path = "trajs/a-best-traj.txt"
+from trajs.a_settings import _n_timesteps, validation_set, training_set, initialLA, pv_stddev, motor_model, la_indices, ha_index
+
+_data_path = "b-2d-merge-medium/data"
+_traj_path = "trajs/b-best-traj.txt"
+from trajs.b_settings import _n_timesteps, validation_set, training_set, initialLA, pv_stddev, motor_model, la_indices, ha_index
+
+_data_path = "c-2d-merge-hard/data"
+_traj_path = "trajs/c-best-traj.txt"
+from trajs.c_settings import _n_timesteps, validation_set, training_set, initialLA, pv_stddev, motor_model, la_indices, ha_index
+
+_data_path = "d-2d-merge-impossible/data"
+_traj_path = "trajs/d-best-traj.txt"
+from trajs.d_settings import _n_timesteps, validation_set, training_set, initialLA, pv_stddev, motor_model, la_indices, ha_index
+
+_data_path = "f-2d-highway/data"
+_traj_path = "trajs/f-best-traj.txt"
+from trajs.f_settings import _n_timesteps, validation_set, training_set, initialLA, pv_stddev, motor_model, la_indices, ha_index
+
+
+
+# get expert data -----------------------------------------------------------------
+def get_single_expert_df(n):
+  return pd.read_csv(_data_path+f"/data{n}.csv", skipinitialspace=True)
+def get_single_expert_traj(n):
+  data = get_single_expert_df(n)
+  all_obs = data[:][:_n_timesteps].to_numpy()
+  return all_obs
+
+
+
+
+# get pred data -------------------
+def get_all_pred_ha():
+  f = open(_traj_path)
+  res = []
+  cur = []
+  n = 0
+  f.readline()
+  while True:
+    line = f.readline()
+    if ha := re.findall("ha, pred1, pred3: \s+ \d  \d  (\d)", line):
+      ha = int(ha[0])
+      cur.append(ha)
+    elif not line:
+      res.append(cur)
+      break
+    else:
+      res.append(cur)
+      cur = []
+      n += 1
+  return res
+
+
+
+def get_err(a_, b_, stdev_):
+  res = [np.log(1-norm.pdf(abs(a-b)/stdev)) for a, b, stdev in zip(a_, b_, stdev_)]
+  return sum(res)
+
+
+def evaluate(single_expert_traj, single_pred_ha):
+  cum_err = 0
+  cum_acc = 0
+  last_la = initialLA
+  for i in range(_n_timesteps):
+    info = single_expert_traj[i]
+    pred_ha = single_pred_ha[i]
+    actual_la = info[la_indices]
+    pred_la = motor_model(pred_ha, info, last_la)
+    err = get_err(actual_la, pred_la, pv_stddev)
+    cum_err += err
+    cum_acc += (info[ha_index]==pred_ha)
+    last_la = actual_la
+  return (cum_err / _n_timesteps, cum_acc / _n_timesteps)
+
+
+
+all_pred_ha = get_all_pred_ha()
+all_expert_traj = [get_single_expert_traj(i) for i in range(validation_set)]
+sum_err = 0
+sum_acc = 0
+for i in range(training_set, validation_set):
+  eval_res = evaluate(all_expert_traj[i], all_pred_ha[i])
+  sum_err += eval_res[0]
+  sum_acc += eval_res[1]
+
+set_size = validation_set - training_set
+print("experiment " + _data_path)
+print("avg ll")
+print(sum_err / set_size)
+print("avg acc")
+print(sum_acc / set_size)
+
---
+++ trajs/f_settings.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+training_set = 10
+validation_set = 30 # including training_set
+
+ha_index = 22
+la_indices = [20,21]
+
+_la_columns = ["LA.steer", "LA.acc"]
+pv_range = [
+    [-0.3, 0.3],
+    [-30, 30]
+]
+pv_stddev = [0.01, 2.0]
+
+numHA = 4
+_n_timesteps = 149
+initialHA = 0
+initialLA = [0,0]
+
+KP_H = 0.5 # Turning rate
+TURN_HEADING = 0.15 # Target heading when turning
+TURN_TARGET = 30 # How much to adjust when targeting a lane (higher = smoother)
+max_velocity = 40 # Maximum velocity
+turn_velocity = 30 # Turning velocity
+
+lane_diff = 4
+def laneFinder(y):
+    return round(y / lane_diff)
+
+def motor_model(ha, data, data_prev):
+    target_acc = 0.0
+    target_heading = 0.0
+    if ha == 0:
+        target_acc = max_velocity - data[2]
+
+        target_y = laneFinder(data[1]) * 4
+        target_heading = np.arctan((target_y - data[1]) / TURN_TARGET)
+    elif ha == 1:
+        target_acc = data[12] - data[2]
+
+        target_y = laneFinder(data[1]) * 4
+        target_heading = np.arctan((target_y - data[1]) / TURN_TARGET)
+    elif ha == 2:
+        target_acc = turn_velocity - data[2]
+        target_heading = -TURN_HEADING
+    else:
+        target_acc = turn_velocity - data[2]
+        target_heading = TURN_HEADING
+
+    target_steer = target_heading - data[4]
+    if(target_steer > data_prev[0]):
+        target_steer = min(target_steer, data_prev[0] + 0.04)
+    else:
+        target_steer = max(target_steer, data_prev[0] - 0.04)
+
+    if(target_acc > data_prev[1]):  
+        target_acc = min(target_acc, data_prev[1] + 4)
+    else:
+        target_acc = max(target_acc, data_prev[1] - 6)
+
+    return [target_steer, target_acc]
